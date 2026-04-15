3809521b63143fd1d42fab2cef41bea33de01b71
Mateus Arruda <mateusarrudasw@gmail.com>
2026-04-15 14:39:40 -0300

    tela do jogo mostra nomes dos jogadores

---
+++ scripts/showPlayerName.js
@@ -0,0 +1,9 @@
+import { Gameboard } from "./tictactoe.js";
+
+const player1name = document.querySelector('.player1name');
+const player2name = document.querySelector('.player2name');
+
+let playerName = Gameboard.getPlayers();
+
+player1name.textContent = playerName[0].name;
+player2name.textContent = playerName[1].name;
